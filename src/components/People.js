b3a6c1f5fd52a21314507450c906f6081669bffb
Efosa Alohan <efosa.alohan@live.com>
2020-12-17 18:36:13 +0100

    Filter character by user name

--- src/components/People.js
+++ src/components/People.js
@@ -1,65 +1,76 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import {Grid, Card} from 'semantic-ui-react';
-import Details from './Details';
+import Details from './Details'
 
-
-
-class People extends Component {
-  constructor(props) {
+class SearchBar extends Component {
+    constructor(props){
     super(props);
     this.state = {
-    people: [],
+      query: "",
+      people: [],
+      token: null,
+      isLoading: true
     };
-  }
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch('https://swapi.dev/api/people')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Something went wrong ...');
-        }
-      })
-      .then(data => this.setState({ people: data.results, isLoading: false }))
-      .catch(error => this.setState({ error, isLoading: false }));
-  }
-
-  render() {
-    const {people, isLoading, error } = this.state;
- 
-    if (error) {
-      return <p>{error.message}</p>;
-    }
- 
-    if (isLoading) {
-      return <p>Loading ...</p>;
-    }
- 
-    return (
-        <div>
-       <h1>People</h1>
-            <Grid columns={4}>
-                {people.map((people, i) =>{
-                    return(
-                        <Grid.Column key={i}>
-                            <Card>
-                                <Card.Content>
-                                    <Card.Header>Name:{people.name}</Card.Header>
-                                    <Card.Description>
-                                    <Details details={people} /> 
-                                    </Card.Description>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
-                    )
-                })}
-            </Grid>
-            
-        </div>
-    );
-  }
-
 }
-
-  export default People;
+    onChange = e => {
+      const { value } = e.target;
+      this.setState({
+        query: value
+      });
+  
+      this.search(value);
+    };
+  
+    search = query => {
+      const url = `https://swapi.dev/api/people?search=${query}`;
+      const token = {};
+      this.token = token;
+       fetch(url)
+        .then(results => results.json())
+        .then(data => {
+          if (this.token === token) {
+            this.setState({ people: data.results,  isLoading: false  });
+          }
+        });
+    };
+  
+    componentDidMount() {
+      this.search("");
+      this.setState({ isLoading: true });
+    }
+  
+    render() {
+      const {people, isLoading} = this.state;
+      
+      if (isLoading) {
+        return <p>Loading ...</p>;
+      }
+      return (
+        <div>
+        <form>
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search for..."
+            onChange={this.onChange}
+          />
+          <Grid columns={4}>
+          {people.map(person => (
+            <ul key={person.name}>
+            <Card>
+            <Card.Content>
+            <Card.Header>Name:{person.name}</Card.Header>
+            <Card.Description>
+            <Details details={person} /> 
+            </Card.Description>
+            </Card.Content>
+            </Card>
+            </ul>
+          ))}
+          </Grid>
+        </form>
+        </div>
+      );
+    }
+}
+export default SearchBar;
---
+++ src/components/Test.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {Grid, Card} from 'semantic-ui-react';
+import Details from './Details';
+
+
+
+class People extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    people: [],
+    isLoading: false,
+    error: null,
+    };
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch('https://swapi.dev/api/people')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => this.setState({ people: data.results, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const {people, isLoading, error } = this.state;
+ 
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+ 
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+ 
+    return (
+        <div>
+       <h1>People</h1>
+            <Grid columns={4}>
+                {people.map((people, i) =>{
+                    return(
+                        <Grid.Column key={i}>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>Name:{people.name}</Card.Header>
+                                    <Card.Description>
+                                    <Details details={people} /> 
+                                    </Card.Description>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    )
+                })}
+            </Grid>
+            
+        </div>
+    );
+  }
+
+}
+
+  export default People;
