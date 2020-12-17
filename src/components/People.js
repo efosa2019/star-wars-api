bd7b4a03244a4ab9d69c81992f696ed3e9369499
Efosa Alohan <efosa.alohan@live.com>
2020-12-17 20:12:41 +0100

    Added some styling

--- src/components/People.js
+++ src/components/People.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import {Grid, Card} from 'semantic-ui-react';
-import Details from './Details'
+import Details from './Details';
 
 class SearchBar extends Component {
     constructor(props){
